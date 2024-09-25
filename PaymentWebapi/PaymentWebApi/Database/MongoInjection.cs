1d639cfdbf5e8aaaa936def6ac61fcbf92dd9ce9
Thales Eduardo <telc_012@hotmail.com>
2024-09-24 21:03:16 -0300

    ajustes mongo db injeção

--- PaymentWebapi/PaymentWebApi/Database/MongoInjection.cs
+++ PaymentWebapi/PaymentWebApi/Database/MongoInjection.cs
@@ -7,6 +7,7 @@ public static class MongoInjection
 {
     public static void AddDatabase(this IServiceCollection services)
     {
+        services.AddSingleton<IConnectionStringBuilderService, ConnectionStringBuilderService>();
         var connectionString = services.BuildServiceProvider().
             GetRequiredService<IConnectionStringBuilderService>().GenerateMongConnection(out string databaseName);
         var mongoClient = new MongoClient(connectionString);
--- PaymentWebapi/PaymentWebApi/Program.cs
+++ PaymentWebapi/PaymentWebApi/Program.cs
@@ -1,14 +1,12 @@
 using PaymentWebApi.Database;
-using PaymentWebApi.Database.ConnectionStringBuilder;
 using PaymentWebApi.Database.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IConnectionStringBuilderService, ConnectionStringBuilderService>();
 builder.Services.AddDatabase();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
